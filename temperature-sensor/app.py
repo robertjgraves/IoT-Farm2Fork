9734f1603e228e9409d6a0fc2f278c96b45e4fd9
Robert Graves <robertjgraves@gmail.com>
2021-12-31 21:38:10 -0800

    convert dictionary to json payload, added clear()

--- temperature-sensor/app.py
+++ temperature-sensor/app.py
@@ -2,19 +2,24 @@ import time
 import paho.mqtt.client as mqtt
 import json
 from seeed_dht import DHT
+from os import system, name
 
 print("Temperature Sensor")
 
 sensor = DHT("11", 5)
 
-id = '264d5b81-aac4-4d74-aa92-9539914e2cbd'
+mqtt_server = '192.168.1.102'               # Ignition server (Raspberry Pi 4)
+#mqtt_server = 'test.mosquitto.org'         # public mosquitto server
+#mqtt_client = '192.168.1.107'              # local mosquitto server
+
+id = 'pi-zero-2w'
 
 client_name = id + 'temperature_sensor_client'
-client_telementry = id + '/telemetry'
+client_telementry = id + '/telemetry/'
 
 mqtt_client = mqtt.Client(client_name)
-mqtt_client.connect('test.mosquitto.org')
-#mqtt_client.connect('192.168.1.107')
+
+mqtt_client.connect(mqtt_server)
 
 mqtt_client.loop_start()
 print("MQTT Connected")
@@ -22,15 +27,38 @@ print("MQTT Connected")
 def CtoF(celsius):
     return celsius * (9.0 / 5.0) + 32.0
 
+def clear():
+
+    # for windows
+    if name == 'nt':
+        _ = system('cls')
+    
+    # for mac and linux (here, os.name is 'posix')
+    else:
+        _ = system('clear')
+
+
 if __name__ == '__main__':
     try:
         while True:
-            hum, temp = sensor.read()
-            tempF = CtoF(temp)
-            telementry = json.dumps({'temperature C' : temp, 'humidity' : hum, 'temperature F' : tempF})
+            clear()
+            print("Temperature Sensor")
+            
+            hum, tempC = sensor.read()
+            tempF = CtoF(tempC)
+
+            data = {}
+            data["tempC"] = tempC
+            data["humidity"] = hum
+            data["tempF"] = tempF
+            data["time"] = time.time()
+
+            print(f'Temperature {tempC:.1f}°C / {tempF:.1f}°F \tHumidity {hum}% \tTime {data["time"]}')
+
+            telementry = json.dumps(data)
+            
             mqtt_client.publish(client_telementry, telementry)
             
-            print(f'Temperature {temp:.1f}°C / {tempF:.1f}°F \tHumidity {hum}%')
             time.sleep(2)
     
     except KeyboardInterrupt:
