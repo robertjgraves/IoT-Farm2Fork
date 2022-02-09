39c0bb625a2c17f66833dd0ed7a9dc55d97a8098
Robert Graves <robertjgraves@gmail.com>
2022-02-08 16:07:35 -0800

    add mqtt connection code

--- soil-moisture-sensor/app.py
+++ soil-moisture-sensor/app.py
@@ -1,7 +1,26 @@
 import time
+import paho.mqtt.client as mqtt
+import json
 from grove.adc import ADC
+from os import system, name
+
+print("Soil Moisture Sensor")
 
 adc = ADC()
+
+mqtt_server = '192.168.1.102'               # Ignition server (Raspberry Pi 4)
+
+id = 'pi-zero-2w'
+
+client_name = id + 'soil_moisture_sensor_client'
+client_telemetry = id + '/telemetry/'
+
+mqtt_client = mqtt.Client(client_name)
+
+mqtt_client.connect(mqtt_server)
+
+mqtt_client.loop_start()
+print("MQTT Connected")
 
 while True:
     soil_moisture = adc.read(0)
